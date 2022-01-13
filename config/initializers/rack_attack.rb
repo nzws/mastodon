e57ac2842c901348534675db4da8d7589e94a8bb
Yuzuki Aida <dev@nzws.me>
2022-01-13 14:39:21 +0000

    Update rate limit again

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -46,7 +46,7 @@ class Rack::Attack
     IpBlock.blocked?(req.remote_ip)
   end
 
-  throttle('throttle_authenticated_api', limit: 300, period: 5.minutes) do |req|
+  throttle('throttle_authenticated_api', limit: 50000000, period: 1.minutes) do |req|
     req.authenticated_user_id if req.api_request?
   end
 
@@ -54,11 +54,11 @@ class Rack::Attack
     req.remote_ip if req.api_request? && req.unauthenticated?
   end
 
-  throttle('throttle_api_media', limit: 30, period: 30.minutes) do |req|
+  throttle('throttle_api_media', limit: 50000000, period: 1.minutes) do |req|
     req.authenticated_user_id if req.post? && req.path.start_with?('/api/v1/media')
   end
 
-  throttle('throttle_media_proxy', limit: 30, period: 10.minutes) do |req|
+  throttle('throttle_media_proxy', limit: 50000000, period: 1.minutes) do |req|
     req.remote_ip if req.path.start_with?('/media_proxy')
   end
 
