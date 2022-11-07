fb6075a8fa6d76fb48ec69b9c01e9c104d46d764
Yuzuki Aida <dev@nzws.me>
2022-11-07 20:06:03 +0900

    Fix config

--- dev.sh
+++ dev.sh
@@ -1,5 +1,11 @@
-docker compose up -d -f docker-compose.dev.yml
-./bin/rails db:setup
+set -eu
+
+docker compose -f docker-compose.dev.yml up -d
+
+bundle install
+yarn install
+
+RAILS_ENV=development ./bin/rails db:setup
 
 gem install foreman
 foreman start -f Procfile.dev
