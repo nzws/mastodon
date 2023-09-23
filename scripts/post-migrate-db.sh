fa028cdb97131a3c562cc3b3307debd953b4ec51
Yuzuki Aida <dev@nzws.me>
2023-09-23 21:07:37 +0900

    Add dev env

--- scripts/post-migrate-db.sh
+++ scripts/post-migrate-db.sh
@@ -1,3 +1,3 @@
 set -eu
 
-docker run --rm -e RAILS_ENV=production --env-file ./scripts/.env.production ghcr.io/nzws/don.nzws.me:production-arm64 bundle exec rake db:migrate
+docker run --rm -e RAILS_ENV=production --env-file ./scripts/.env.production.nzws ghcr.io/nzws/don.nzws.me:production-arm64 bundle exec rake db:migrate
